6424acb8abac96b2b81aa17278051b0a1cf824e8
Andrey Rodionov <roand@inbox.ru>
2018-12-30 22:13:28 +0300

    Вспомогательные файлы cmake собирает в директорию cmake

--- cmake/shared.cmake
+++ cmake/shared.cmake
@@ -7,17 +7,16 @@ set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${BINARY_DIR}/${RLIBRARYPATH}")
 
 # Сборка проекта
 include("${CMAKE_SOURCE_DIR}/cmake/rpath.cmake")
-add_library(${PROJECT_NAME} SHARED ${HEADERS} ${SOURCES} ${FORMS} ${RESOURCES})
+add_library(${PROJECT_NAME} STATIC ${HEADERS} ${SOURCES} ${FORMS} ${RESOURCES})
 target_include_directories(${PROJECT_NAME}
     PUBLIC ${PROJECT_SOURCE_DIR}/include
 )
 target_link_libraries(${PROJECT_NAME} ${LIBRARIES})
 
 # Установка проекта
-install(TARGETS ${PROJECT_NAME} DESTINATION ${RLIBRARYPATH})
+install (TARGETS ${PROJECT_NAME} DESTINATION ${RLIBRARYPATH})
 file(GLOB PUBHEADERS   "include/${PROJECT_NAME}/*.h")
 foreach(PUBFILENAME ${PUBHEADERS})
     string(REPLACE "include/${PROJECT_NAME}/" "" FILENAME ${PUBFILENAME})
-    install(FILES ${FILENAME} DESTINATION ${INCPATH}/${PROJECT_NAME})
+    install(FILES ${FILENAME} DESTINATION ${INCPATH})
 endforeach()
-
